6266550186ccc7dc4d0ec1a051bccd0499d14f89
Ulzahk <ulzahk@gmail.com>
2021-08-08 10:20:32 -0500

    [UPDATE] Added fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,20 @@ module.exports = {
         test: /\.png/,
         type: "asset/resource",
       },
+      {
+        test: /\.(worff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
